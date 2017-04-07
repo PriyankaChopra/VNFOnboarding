74bb6f3af98eead58b347c07b6ab34e606d55e5b
root <root@verizon.com>
2017-04-07 21:17:14 +0530

    initial commit

---
+++ src/main/java/com/verizon/vnf/VnfApplication.java
@@ -0,0 +1,12 @@
+package com.verizon.vnf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnfApplication.class, args);
+	}
+}
